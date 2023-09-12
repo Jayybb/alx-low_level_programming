417447517ca30bf75d915204779de61fc5677bda
Jayybb <inyangj6@gmail.com>
2023-09-12 14:00:12 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,31 @@
 #include "main.h"
 
+/**
+ * print_sign - Printys sum
+ * @n: The nmber
+ * Return: 1 for all
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
+
+#include "main.h"
+
 
 /**
   * main - check the code.
@@ -9,7 +35,7 @@
 int main(void)
 {
 	int r;
-	r = print_sign(98);
+	 r = print_sign(98);
 	_putchar(',');
 	_putchar(' ');
 	_putchar(r + '0');
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _putchar(char c);
-void print_times_table(int n);
+int print_sign(int n);
 
 #endif
