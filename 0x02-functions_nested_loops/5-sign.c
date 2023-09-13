c3270dc5d9f1213beb4db08e85c09324c994ea36
Jayybb <inyangj6@gmail.com>
2023-09-12 20:20:36 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,17 +10,30 @@ int print_sign(int n)
 
 {
 	if (n > 0)
+
 	{
+
 	_putchar(43);
 	return (1);
+
 	}
+
+
 	else if (n < 0)
+
+	{
+
 	_putchar(45);
 	return (-1);
+
 	}
+
 	else
+
 	{
+
 	_putchar(45);
 	return (0);
+
 	}
 }
