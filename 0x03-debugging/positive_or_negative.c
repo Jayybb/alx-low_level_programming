f5cf0e29436060ce37bad7966782c5249f7df436
Jayybb <inyangj6@gmail.com>
2023-09-14 04:01:49 -0700

    0-main.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,25 +1,18 @@
-#include <stdlib.h>
+#include "main.h"
 #include <stdio.h>
-#include <time.h>
 
-
-/**
-* main - Prints a random number decides whether it is positive,negative,orzero.
-*
-* Return: Always 0.
-*/
-int main(void)
+void positive_or_negative(int i)
 {
-int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
-printf("%d is positive\n", n);
-
-else if (n < 0)
-printf("%d is negative\n", n);
+if (i > 0)
+{
+printf("%d is positive\n", i);
+}
+else if (i < 0)
+printf("%d is negative\n", i);
 else
-printf("%d is zero\n", n);
-return (0);
+
+printf("%d is zero\n", i);
+
+
 }
