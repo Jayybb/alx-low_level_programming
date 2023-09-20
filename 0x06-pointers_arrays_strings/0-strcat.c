d333752284563dad0c9846e2b049ec0d3781fa8f
Jayybb <inyangj6@gmail.com>
2023-09-20 10:37:32 -0700

    jjj

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate two strings
+ * dest - sample one
+ * src - sample two
+ * Return : dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int dest = i;
+int src = j;
+i = 0;
+j = 0;
+
+for
+	(dest[i] != '\0');
+{
+	i++;
+}
+for
+	(src[j] != '\0');
+
+{
+dest[i] = src[j];
+	i++;
+	j++;
+}
+	dest[i] = '\0';
+return (dest);
+}
