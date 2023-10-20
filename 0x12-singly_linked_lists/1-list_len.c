034785a0a3651ab49af446920fd309d5c32e344c
Jayybb <inyangj6@gmail.com>
2023-10-20 03:41:06 -0700

    list len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - length of the list
+ * @h: header to the first node
+ * Return: Number of the nodes in the list
+ */
+
+size_t list_len(const list_t *h)
+{
+int i;
+
+if (h == 0)
+return (0);
+
+for (i = 1; h->next != 0; i++);
+h = h->next;
+
+return (i);
+}
