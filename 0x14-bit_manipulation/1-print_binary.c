c1dfcc4c56f47fe4b34accd28d62a118ade0c353
Jayybb <inyangj6@gmail.com>
2023-10-26 12:56:58 -0700

    1 get

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,4 @@
-#include "maim.h"
+#include "main.h"
 
 /**
  * print_binary - function that prints binary representation of number
@@ -7,4 +7,24 @@
  * Return: void
  */
 void print_binary(unsigned long int n)
+{
+unsigned long int divisor, check;
+char i;
 
+i = 0;
+divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
+while (divisor != 0)
+{
+check = n & divisor;
+if (check == divisor)
+{
+i = 1;
+_putchar('1');
+}
+else if (i == 1 || divisor == 1)
+{
+_putchar('0');
+}
+divisor >>= 1;
+}
+}
