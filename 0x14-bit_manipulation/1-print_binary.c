84a081ba63f76e7f05da47c0ef67044a3a757d76
Jayybb <inyangj6@gmail.com>
2023-10-26 11:15:04 -0700

    2 get

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,10 @@
+#include "maim.h"
+
+/**
+ * print_binary - function that prints binary representation of number
+ * @n: number to be printed
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+
