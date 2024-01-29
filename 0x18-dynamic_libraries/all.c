1e878e6b29278d836ebf51c8b65dbed66e1c8ad0
Jayybb <inyangj6@gmail.com>
2024-01-29 06:02:33 -0800

    100-operations.so

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,57 @@
+
+/**
+ * add - to aggregate values
+ * @a: fist int
+ * @b: second int
+ * Return: addition
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * mul - multiply values
+ * @a: first int
+ * @b: second int
+ * Return: multiplication
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * mod - mod integers
+ * @a: first int
+ * @b: second int
+ * Return: final answer
+ */
+int mod(int a, int b)
+{
+return (a % b);
+}
+
+/**
+ * sub - subtract integers
+ * @a: first int
+ * @b: second int
+ * Return: subtraction
+ */
+int sub(int a, int b)
+{
+return (a - b);
+}
+
+#include <stdio.h>
+
+/**
+ * div - divide integers
+ * @a: first int
+ * @b: second int
+ * Return: division
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
