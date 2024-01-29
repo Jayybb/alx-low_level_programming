56dcd29a454d818e1b0f01ee20a6914d6eafba16
Jayybb <inyangj6@gmail.com>
2024-01-29 04:46:34 -0800

    dynamic

---
+++ 0x18-dynamic_libraries/functions/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: the main string
+ * @needle: the substring
+ * Return: where the substring is in the main string or 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+ int i;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		i = 0;
+
+		if (haystack[i] == needle[i])
+		{
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+				i++;
+			} while (haystack[i] == needle[i]);
+		}
+		haystack++;
+	}
+	return ('\0');
+}
